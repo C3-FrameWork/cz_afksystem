0997b8fa766f70dc2acf18a8995bab3c8558349a
C3h1cDevelopment <128829862+C3-FrameWork@users.noreply.github.com>
2023-07-11 13:33:44 +0200

    1.0 - Release

---
+++ server.lua
@@ -0,0 +1,4 @@
+RegisterServerEvent("kickForBeingAnAFKDouchebag")
+AddEventHandler("kickForBeingAnAFKDouchebag", function()
+	DropPlayer(source, "Vi ste zelo dolgo ze AFK, premaknite se ali pa boste odstranjeni iz mesta!")
+end)
